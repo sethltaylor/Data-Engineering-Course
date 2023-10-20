30409d77680ed2b74ca0044340423427bed4d8f5
sethltaylor <setht1191@gmail.com>
2023-10-19 23:03:22 -0400

    Update ingest_data.py
    Updating ingest_data.py to incorporate prefect flows and to use prefect sqlalchemy block to streamline data ingestion script.

--- Docker/ingest_data.py
+++ Docker/ingest_data.py
@@ -1,42 +1,59 @@
-import pandas as pd
 import os
-from sqlalchemy import create_engine
 import argparse
+from time import time
+import pandas as pd
+from sqlalchemy import create_engine
+from prefect import flow, task 
+from prefect.tasks import task_input_hash
+from datetime import timedelta
+from prefect_sqlalchemy import SqlAlchemyConnector
 
-def main(params):
-    user = params.user
-    password = params.password
-    host = params.host 
-    port = params.port 
-    db = params.db
-    table_name = params.table_name
-    url = params.url
+@task(log_prints=True,retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
+def extract_data(url):
+     # the backup files are gzipped, and it's important to keep the correct extension
+    # for pandas to be able to open the file
+    if url.endswith('.csv.gz'):
+        csv_name = 'yellow_tripdata_2021-01.csv.gz'
+    else:
+        csv_name = 'output.csv'
 
-    
-    os.system(f"curl {url} -o output.parquet")
+    os.system(f"wget {url} -O {csv_name}")
+  
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
-    df = pd.read_parquet('output.parquet', engine = 'pyarrow')
+    df = next(df_iter)
 
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    return df
 
-    df.to_sql(name= table_name, con=engine, if_exists='replace')
+@task(log_prints=True)
+def transform_data(df):
+    print(f"pre: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+    df = df[df['passenger_count'] !=0]
+    print(f"post: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+    return df
 
+@task(log_prints=True, retries = 3)
+def ingest_data(table_name, df):
+    connection_block = SqlAlchemyConnector.load('nyc-taxi-connector')
+
+    with connection_block.get_connection(begin=False) as engine:
+        df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+
+@flow(name="Subflow",log_prints = True)
+def log_subflow(table_name:str):
+    print("Logging Subflow for: {table_name}")
+
+@flow(name = "Ingest Flow")
+def main_flow(table_name: str):
+    csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+    log_subflow(table_name)
+    raw_data = extract_data(csv_url)
+    data = transform_data(raw_data)
+    ingest_data(table_name, data)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Ingest data to Postgres')
-
-    parser.add_argument('--user', required=True, help='user name for postgres')
-    parser.add_argument('--password', required=True, help='password for postgres')
-    parser.add_argument('--host', required=True, help='host for postgres')
-    parser.add_argument('--port', required=True, help='port for postgres')
-    parser.add_argument('--db', required=True, help='database name for postgres')
-    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the file')
-
-    args = parser.parse_args()
-
-    main(args)
-
-
-
-
+    main_flow("yellow_taxi_trips")
+    
